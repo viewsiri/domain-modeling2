02074b619ac9c8d1d190768b3c46adf1016d9d10
Violet Sirikajornkij <thitirat.sirikajornkij@kellogg.northwestern.edu>
2024-04-12 20:27:05 +0000

    school

--- school/school.sql
+++ school/school.sql
@@ -14,6 +14,31 @@ CREATE TABLE students (
   phone_number TEXT
 );
 
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+);
+
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT,
+  course_id INTEGER,
+  teacher_id INTEGER
+);
+
+CREATE TABLE enrollment (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  student_id INTEGER,
+  section_id INTEGER
+);
 -- Create the rest of the tables
 
 
